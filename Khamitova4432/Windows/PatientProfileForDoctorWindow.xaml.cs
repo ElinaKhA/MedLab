24190ed0bc5c4e3204cd517a99c8ccd43bec31b0
Элина Хамитова <khamitovaelina09@gmail.com>
2024-02-25 00:05:18 +0300

    окна доктора для работы с пациентами

---
+++ Khamitova4432/Windows/PatientProfileForDoctorWindow.xaml.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Khamitova4432.Windows
+{
+    /// <summary>
+    /// Логика взаимодействия для PatientProfileForDoctorWindow.xaml
+    /// </summary>
+    public partial class PatientProfileForDoctorWindow : Window
+    {
+        public PatientProfileForDoctorWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void BackBtn_Click(object sender, RoutedEventArgs e)
+        {
+            WorkWithPatientsWindow pw = new WorkWithPatientsWindow();
+            pw.Show();
+            Close();
+        }
+
+        private void ExitBtn_Click(object sender, RoutedEventArgs e)
+        {
+            MainWindow mw = new MainWindow();
+            mw.Show();
+            Close();
+        }
+
+        private void RiskWinBtn_Click(object sender, RoutedEventArgs e)
+        {
+            RiscCalculateWindow rw = new RiscCalculateWindow(1, 1);
+            rw.Show();
+            Close();
+        }
+
+        private void TreatPlanWinBtn_Click(object sender, RoutedEventArgs e)
+        {
+            TreatmentPlanDoctorWindow tw = new TreatmentPlanDoctorWindow();
+            tw.Show();
+            Close();
+        }
+
+        private void AddLabResultsWinBtn_Click(object sender, RoutedEventArgs e)
+        {
+            LabResultsAddWindow lw = new LabResultsAddWindow();
+            lw.Show();
+            Close();
+        }
+
+        private void AppointmentWinBtn_Click(object sender, RoutedEventArgs e)
+        {
+            AppointmentWindow aw = new AppointmentWindow(1,1);
+            aw.Show();
+            Close();
+        }
+    }
+}
---
+++ Khamitova4432/Windows/WorkWithPatientsWindow.xaml.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Khamitova4432.Windows
+{
+    /// <summary>
+    /// Логика взаимодействия для WorkWithPatientsWindow.xaml
+    /// </summary>
+    public partial class WorkWithPatientsWindow : Window
+    {
+        public WorkWithPatientsWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void RegPatientBtn_Click(object sender, RoutedEventArgs e)
+        {
+            RegistrationWindow rw = new RegistrationWindow();
+            rw.Show();
+            Close();
+        }
+
+        private void ExitBtn_Click(object sender, RoutedEventArgs e)
+        {
+            MainWindow mw = new MainWindow();
+            mw.Show();
+            Close();
+        }
+
+        private void BackBtn_Click(object sender, RoutedEventArgs e)
+        {
+            DoctorWindow dw = new DoctorWindow();
+            dw.Show();
+            Close();
+        }
+
+        private void NextBtn_Click(object sender, RoutedEventArgs e)
+        {
+            PatientProfileForDoctorWindow pw = new PatientProfileForDoctorWindow();
+            pw.Show();
+            Close();
+        }
+    }
+}
