93d465cadb217132ec9336aa632c9f04c355dbe9
Элина Хамитова <khamitovaelina09@gmail.com>
2024-02-25 00:04:22 +0300

    Окна для работы с анализами

---
+++ Khamitova4432/Windows/LabResultsAddWindow.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Khamitova4432.Windows
+{
+    /// <summary>
+    /// Логика взаимодействия для LabResultsAddWindow.xaml
+    /// </summary>
+    public partial class LabResultsAddWindow : Window
+    {
+        public LabResultsAddWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void BackBtn_Click(object sender, RoutedEventArgs e)
+        {
+            PatientProfileForDoctorWindow pw = new PatientProfileForDoctorWindow();
+            pw.Show();
+            Close();
+        }
+
+        private void AddBtn_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void ExitBtn_Click(object sender, RoutedEventArgs e)
+        {
+            MainWindow mw = new MainWindow();
+            mw.Show();
+            Close();
+        }
+    }
+}
---
+++ Khamitova4432/Windows/LabResultsWindow.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Khamitova4432.Windows
+{
+    /// <summary>
+    /// Логика взаимодействия для LabResultsWindow.xaml
+    /// </summary>
+    public partial class LabResultsWindow : Window
+    {
+        public LabResultsWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void BackBtn_Click(object sender, RoutedEventArgs e)
+        {
+            PatientWindow pw = new PatientWindow();
+            pw.Show();
+            Close();
+        }
+
+        private void ExitBtn_Click(object sender, RoutedEventArgs e)
+        {
+            MainWindow mw = new MainWindow();
+            mw.Show();
+            Close();
+        }
+    }
+}
