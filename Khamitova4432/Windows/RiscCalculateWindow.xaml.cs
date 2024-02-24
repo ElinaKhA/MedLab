e942cb01168e117e3bf1bd7cd1ae295c8495c94c
Элина Хамитова <khamitovaelina09@gmail.com>
2024-02-25 00:07:28 +0300

    окно вычисления риска

---
+++ Khamitova4432/Windows/RiscCalculateWindow.xaml.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Khamitova4432.Windows
+{
+    /// <summary>
+    /// Логика взаимодействия для RiscCalculateWindow.xaml
+    /// </summary>
+    public partial class RiscCalculateWindow : Window
+    {
+        int identifr;
+        public RiscCalculateWindow(int idrole, int id)
+        {
+            InitializeComponent();
+            identifr= idrole;
+        }
+
+        private void ExitBtn_Click(object sender, RoutedEventArgs e)
+        {
+            MainWindow mw = new MainWindow();
+            mw.Show();
+            Close();
+        }
+
+        private void BackBtn_Click(object sender, RoutedEventArgs e)
+        {
+            if (identifr==0)
+            {
+                PatientWindow pw = new PatientWindow();
+                pw.Show();
+                Close();
+            }
+            else
+            {
+                PatientProfileForDoctorWindow dw = new PatientProfileForDoctorWindow();
+                dw.Show();
+                Close();
+            }
+        }
+
+        private void CalculateBtn_Click(object sender, RoutedEventArgs e)
+        {
+            //Записывать риск в бд
+            if (identifr == 0)
+            {
+                PatientWindow pw = new PatientWindow();
+                pw.Show();
+                Close();
+            }
+            else
+            {
+                PatientProfileForDoctorWindow dw = new PatientProfileForDoctorWindow();
+                dw.Show();
+                Close();
+            }
+        }
+    }
+}
