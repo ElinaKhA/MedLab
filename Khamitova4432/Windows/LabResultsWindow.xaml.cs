08c0eb3438f5f25c476d55158e783c4086548b92
Элина Хамитова <khamitovaelina09@gmail.com>
2024-02-28 18:57:52 +0300

    вывод данных результатов анализов реализован

--- Khamitova4432/Windows/LabResultsWindow.xaml.cs
+++ Khamitova4432/Windows/LabResultsWindow.xaml.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.Data.Entity;
+using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -22,20 +23,58 @@ namespace Khamitova4432.Windows
     public partial class LabResultsWindow : Window
     {
         Patient pforwin;
+        medicdbContext _con = new medicdbContext();
         public LabResultsWindow(Patient patient)
         {
             InitializeComponent();
-            pforwin = patient;
-            fiolb.Content = $"{patient.Surname} {patient.Name} {patient.LastName}";
-            var lastRisk = patient.Risks.OrderByDescending(r => r.DateOfCalculated).FirstOrDefault();
-            if (lastRisk != null)
+            try
             {
-                risklb.Content = $"Риск: {lastRisk.CalculatedRisk}";
+                pforwin = patient;
+                fiolb.Content = $"{patient.Surname} {patient.Name} {patient.LastName}";
+                var lastRisk = patient.Risks.OrderByDescending(r => r.DateOfCalculated).FirstOrDefault();
+                if (lastRisk != null)
+                {
+                    risklb.Content = $"Риск: {lastRisk.CalculatedRisk}";
+                }
+                else
+                {
+                    risklb.Content = "Риск не вычислен";
+                }
+                var lastLabResult = _con.LabResults
+                        .Where(lr => lr.PatientId == patient.Id)
+                        .OrderByDescending(lr => lr.DateOfResults)
+                        .FirstOrDefault();
+                if (lastLabResult != null)
+                {
+                    mdrdlb.Content = lastLabResult.SKF;
+                    glucoselb.Content = lastLabResult.Glucose;
+                    sistlb.Content = lastLabResult.SystolicBloodPressure;
+                    diastlb.Content = lastLabResult.DiastolicBloodPressure;
+                    bmilb.Content = lastLabResult.BMI;
+                    holesterollb.Content = lastLabResult.Cholesterol;
+                    if (lastLabResult.Smoking == 0)
+                    {
+                        smolb.Content = "Никогда не курил";
+                    }
+                    else if (lastLabResult.Smoking == 1)
+                    {
+                        smolb.Content = "Раньше курил";
+                    }       
+                    else if (lastLabResult.Smoking == 2)
+                    {
+                        smolb.Content = "Да";
+                    }
+                    if (lastLabResult.Hypertension == 0)
+                    {
+                        hyperlb.Content = "Нет";
+                    } 
+                    else if (lastLabResult.Hypertension == 1)
+                    {
+                        hyperlb.Content = "Да";
+                    }
+                }
             }
-            else
-            {
-                risklb.Content = "Риск не вычислен";
-            }
+            catch { MessageBox.Show("Ошибка"); }
         }
 
         private void BackBtn_Click(object sender, RoutedEventArgs e)
